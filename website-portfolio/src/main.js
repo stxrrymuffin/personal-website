99ad627be4a4effefdc6470cbe1bb5b94b3b0cfb
stxrrymuffin <jessica.chen270412@gmail.com>
2025-09-03 02:22:44 -0400

    loading in cat test model !!
    goodbye cube

--- website-portfolio/src/main.js
+++ website-portfolio/src/main.js
@@ -1,28 +1,56 @@
 import './style.scss'
 import * as THREE from 'three';
-
-const width = window.innerWidth;
-const height = window.innerHeight;
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import {DRACOLoader} from 'three/addons/loaders/DRACOLoader.js';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 
 const canvas = document.querySelector("#exp-canvas");
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+//load in models
+const textureLoader = new THREE.TextureLoader();
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("/draco/")
+
+const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
+
+// TODO: load in textures...
+
+loader.load("/models/cat_compressed.glb", (glb) => {
+  scene.add(glb.scene);
+})
+
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
-renderer.setSize( width, height );
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
 
 camera.position.z = 5;
 
-const render = () => {
+const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
+renderer.setSize( sizes.width, sizes.height );
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setClearColor(0xffffff, 1);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.update();
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+const render = () => {
+  controls.update();
 
   renderer.render( scene, camera );
   window.requestAnimationFrame(render);
